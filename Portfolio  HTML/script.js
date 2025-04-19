3be471457144dfe04460812b256a1c62232aed90
Prajwal Jamkattel <pjamakattel@gmail.com>
2025-04-20 02:45:26 +1000

    Add files via upload

---
+++ Portfolio  HTML/script.js
@@ -0,0 +1,27 @@
+const toggleMenu = () => {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 10) {
+        nav.style.boxShadow = '0 4px 12px rgba(0, 0, 0, 0.1)';
+    } else {
+        nav.style.boxShadow = '0 2px 10px rgba(0, 0, 0, 0.1)';
+    }
+});
